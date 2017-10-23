fa28b5dc1cadb97cc066cd9fac4281b26c35b8c8
Julio César Gómez Naranjo - Ceiba Software <julio.gomez@ceiba.com.co>
2017-10-23 15:41:27 -0500

    Creación del proyecto

---
+++ src/main/java/com/ceiba/Parqueadero/ServicioRestParqueadero/ServicioRestParqueaderoApplication.java
@@ -0,0 +1,12 @@
+package com.ceiba.Parqueadero.ServicioRestParqueadero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioRestParqueaderoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioRestParqueaderoApplication.class, args);
+	}
+}
